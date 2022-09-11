007855afabec9d99dd4f045e4475ea193d646475
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-11 09:41:53 +0200

    3rd commit

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ img = cv2.imread('test.jpg')
 grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(grey, 1.1, 4)
+
+# drawing rectangle around the face
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y) + (x + w, y + h), (255, 0, 0), 2)
