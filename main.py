c8973793ea6f3d8eca45ad0af6fd5f8cfc6ef2b5
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-11 09:36:24 +0200

    2nd commit

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+img = cv2.imread('test.jpg')
+
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(grey, 1.1, 4)
