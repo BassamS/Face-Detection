ce9b3be2d6c9d25a631d2fdaac9b5120a28e0926
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-11 10:04:50 +0200

    final commit

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('test.jpg')
 
-grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-faces = face_cascade.detectMultiScale(grey, 1.1, 4)
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
 # drawing rectangle around the face
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y) + (x + w, y + h), (255, 0, 0), 2)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (225, 0, 0), 2)
+
+cv2.imshow('img', img)
+cv2.waitKey()
+
+cv2.imwrite('face_detected.jpg', img)
